5aef0ed0a6383a29e97385bae724fd851b5b932b
Etan Imanol <etanimanolcastro@gmail.com>
2025-02-26 23:48:04 +0000

    Batch commit: ultimate-addons-for-gutenberg, redux-framework, maintenance, wp-multibyte-patch, code-snippets, autoptimize, sg-security, safe-svg, image-optimization, google-listings-and-ads, imagify, breadcrumb-navxt, smart-slider-3, better-wp-security, coming-soon, woocommerce-payments, woocommerce-gateway-stripe, polylang, flamingo, premium-addons-for-elementor

---
+++ polylang/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wpsyntex/polylang',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '989580ddcaa5912e846ae0dfaacea8b888d634d8',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'wpsyntex/polylang' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '989580ddcaa5912e846ae0dfaacea8b888d634d8',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
