436ddbfbed7b44102a7f3f5132947a4657dddd9c
Etan Imanol <etanimanolcastro@gmail.com>
2025-02-26 22:41:39 +0000

    gp-additional-links

---
+++ gp-additional-links/.svn/pristine/16/166e9cd54bb0476fc0efb5bf652cbd82c1017760.svn-base
@@ -0,0 +1,29 @@
+=== GP Additional Links ===
+Contributors: GregRoss
+Donate link: http://toolstack.com/donate
+Plugin URI: http://glot-o-matic.com/gp-additional-links
+Author URI: http://toolstack.com
+Tags: translation, glotpress
+Requires at least: 4.4
+Tested up to: 6.4.1
+Stable tag: 1.0
+License: GPLv2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+A plugin for GlotPress as a WordPress plugin that adds a link to the WordPress dashboard for admins in the GlotPress page as well as a link to the GlotPress page in the WordPress admin menu.
+
+== Description ==
+
+A plugin for [GlotPress as a WordPress plugin](https://github.com/GlotPress/GlotPress-WP) that adds a link to the WordPress dashboard for admins in the GlotPress page as well as a link to the GlotPress page in the WordPress admin menu.
+
+== Installation ==
+
+Install from the WordPress plugin directory.
+
+== Frequently Asked Questions ==
+
+= TBD =
+
+TBD
+
+== Changelog ==
---
+++ gp-additional-links/gp-additional-links.php
@@ -0,0 +1,66 @@
+<?php
+/*
+Plugin Name: GP Additional Links
+Plugin URI: http://glot-o-matic.com/gp-additional-links
+Description: Add additional links to the GlotPress side menu and the WordPress admin menu.
+Version: 1.0
+Author: GregRoss
+Author URI: http://toolstack.com
+Tags: glotpress, glotpress plugin, translate
+License: GPLv2 or later
+*/
+
+class GP_Additional_Links {
+	public $id = 'additional-links';
+
+	public function __construct() {
+		
+		// Add the dashboard link to the side menu.
+		add_filter( 'gp_nav_menu_items', array( $this, 'gp_nav_menu_items' ), 10, 2 );
+		
+		// Add the admin page to the WordPress settings menu.
+		add_action( 'admin_menu', array( $this, 'admin_menu' ), 10, 1 );
+	}
+	
+	public function gp_nav_menu_items( $items, $location ) {
+		$new = array();
+		
+		if( $location == 'side' ) {
+			$new[admin_url()] = __('Dashboard');
+		}
+		
+		return array_merge( $new, $items );
+	}
+	
+	// This function adds the admin settings page to WordPress.
+	public function admin_menu() {
+		GLOBAL $menu;
+
+		$image = plugins_url( '/GlotPress-Logo-20px.png', __FILE__ );
+		
+		// Add the menu to the admin menu.
+		add_menu_page(__('GlotPress'), __('GlotPress'), 'read', __FILE__, array( $this, 'redirect_to_glotpress'), $image, 1 );
+		
+		// We're going to hack the menu info to use a link to the front end instead of calling the 'redirect_to_glotpress' function so we save a page load.
+		foreach( $menu as $tag => $mi ) {
+			if( $mi[0] == __( 'GlotPress' ) ) {
+				$menu[$tag][2] = gp_url_public_root();
+			}
+		}
+		
+	}
+
+	public function redirect_to_glotpress() {
+		// Just a placeholder, we're going to replace the function call with a real link in the menu_order hook.
+	}
+}
+
+// Add an action to WordPress's init hook to setup the plugin.  Don't just setup the plugin here as the GlotPress plugin may not have loaded yet.
+add_action( 'gp_init', 'gp_additional_links_init' );
+
+// This function creates the plugin.
+function gp_additional_links_init() {
+	GLOBAL $gp_additional_links;
+	
+	$gp_additional_links = new GP_Additional_Links;
+}
