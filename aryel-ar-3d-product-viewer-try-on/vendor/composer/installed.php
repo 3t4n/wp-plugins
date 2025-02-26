b20da314e8c067210305a74524f92cee8ed263ee
Etan Imanol <etanimanolcastro@gmail.com>
2025-02-26 08:18:38 +0000

    aryel-ar-3d-product-viewer-try-on

---
+++ aryel-ar-3d-product-viewer-try-on/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0dd2248d185e9f6b05dba21d248bfc65ff26b600',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0dd2248d185e9f6b05dba21d248bfc65ff26b600',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
