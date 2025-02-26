c2326c147eda63913462cf0a9fc5d1a68c6d8049
Etan Imanol <etanimanolcastro@gmail.com>
2025-02-26 23:28:36 +0000

    Batch commit: oik-weightcountry-shipping, jquery-image-lazy-loading, civist, upcoming-events-lists, tp-education, socketlabs, dashboard-commander, easy-post-types-fields, bus-ticket-booking-with-seat-reservation, wpc-smart-messages, rest-api-custom-fields, woocommerce-auto-restore-stock, interactive-world-map, upload-svg, interactive-maps, unique-title-checker, gf-uploads-as-attachments, wp-supersized, expandcollapse-funk, bd-courier-order-ratio-checker

---
+++ easy-post-types-fields/dependencies/barn2/barn2-lib/build/js/admin/wc-settings.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace Barn2\Plugin\Easy_Post_Types_Fields\Dependencies;
+
+return array('dependencies' => array(), 'version' => '88b8ad03bd0cd6467943');
---
+++ easy-post-types-fields/dependencies/barn2/barn2-lib/build/js/jquery-tiptip/jquery.tipTip.asset.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace Barn2\Plugin\Easy_Post_Types_Fields\Dependencies;
+
+return array('dependencies' => array(), 'version' => 'ca2df7799aca15672ec5');
