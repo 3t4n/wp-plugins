64f48340f7b050665092a2b1cf62435cfb08497c
Etan Imanol <etanimanolcastro@gmail.com>
2025-02-26 06:55:18 +0000

    apoyl-video

---
+++ apoyl-video/includes/deactivator.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * @link       http://www.girltm.com/
+ * @since      1.0.0
+ * @package    Apoyl_Video
+ * @subpackage Apoyl_Video/includes
+ * @author     凹凸曼 <3201361925@qq.com>
+ *
+ */
+class Apoyl_Video_Deactivator {
+
+	
+	public static function deactivate() {
+
+	}
+
+}
