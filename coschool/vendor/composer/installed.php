c419213327933d5f66cb7f3a526df35b9bca1761
Etan Imanol <etanimanolcastro@gmail.com>
2025-02-26 19:20:14 +0000

    coschool

---
+++ coschool/vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'f5869bf210808c25698e90412a6e4625f7dd4cd9',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'f5869bf210808c25698e90412a6e4625f7dd4cd9',
+            'dev_requirement' => false,
+        ),
+        'codexpert/plugin' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../codexpert/plugin',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => '70ff2a318bbde3b22b3cc8a1ef6f2f65a20d953b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
