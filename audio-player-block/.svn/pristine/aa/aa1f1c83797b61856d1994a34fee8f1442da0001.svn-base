43562ebba74c93fdd2caddc5fd90a8c77dce2400
Etan Imanol <etanimanolcastro@gmail.com>
2025-02-26 06:29:16 +0000

    audio-player-block

---
+++ audio-player-block/.svn/pristine/aa/aa1f1c83797b61856d1994a34fee8f1442da0001.svn-base
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => 'e3480717e926600d00c5');
---
+++ audio-player-block/build/admin-help.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => 'a56890a1ba8451d4e48f');
