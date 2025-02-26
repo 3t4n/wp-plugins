cc8e5bd78cf473d5dfb1537c1640d2451797284a
Etan Imanol <etanimanolcastro@gmail.com>
2025-02-26 11:04:10 +0000

    antom-payments

---
+++ antom-payments/assets/blocks/card/card.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('jquery', 'react', 'wc-blocks-registry', 'wc-settings', 'wp-block-editor', 'wp-html-entities', 'wp-i18n'), 'version' => 'd2710bc11b65d5d1c21d');
