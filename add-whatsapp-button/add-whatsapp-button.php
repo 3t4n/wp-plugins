94c7c726921288cfc8cd3cba136bb0ab368dacc2
Etan Imanol <etanimanolcastro@gmail.com>
2025-02-26 12:44:30 +0000

    add-whatsapp-button

---
+++ add-whatsapp-button/add-whatsapp-button.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Plugin Name: Add Chat App Button
+ * Description: Adds a Floating button to your website which opens a WhatsApp chat window
+ * Author: Udi Dollberg
+ * Text Domain: add-whatsapp-button
+ * Domain Path: /languages
+ * Version: 2.1.8
+ * Author URI: http://udidollberg.com/
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+require_once( __DIR__ . '/plugin.php' );
