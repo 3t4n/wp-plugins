d0a98a5533c166c59e9683ba5d39cac477114fb6
Etan Imanol <etanimanolcastro@gmail.com>
2025-02-26 18:50:30 +0000

    bridgedd

---
+++ bridgedd/.svn/pristine/23/23117504f3aa2db21fde318ab4abc74b309af6b2.svn-base
@@ -0,0 +1,20 @@
+=== BridgeDD ===
+Contributors: DionDesigns
+Tags: bridge,integration,phpBB
+Requires at least: 3.9
+Tested up to: 5.9
+Stable tag: trunk
+
+BridgeDD has been discontinued.
+
+== Description ==
+
+[**BridgeDD has been discontinued.**](https://bridgedd.com/support/viewtopic.php?t=2138)
+
+== Installation ==
+
+[**BridgeDD has been discontinued.**](https://bridgedd.com/support/viewtopic.php?t=2138)
+
+== Changelog ==
+
+[**BridgeDD has been discontinued.**](https://bridgedd.com/support/viewtopic.php?t=2138)
---
+++ bridgedd/bridgedd.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: BridgeDD
+Plugin URI: https://bridgedd.com/support
+Version: 1.0
+Author: Dion Designs
+Description: Creates a bridge between phpBB and WordPress
+*/
+if (!defined('WPINC')) {
+	die('ERROR 000');
+}
+
+wp_die('<style>pre{overflow:auto}#buttondiv{margin:-45px 0 -10px;line-height:1;font-size:2em;font-weight:bold;text-align:center}</style><div id="buttondiv"><br /><a href="https://bridgedd.com/support/viewtopic.php?t=2138">BridgeDD has been discontinued</a><br><br><br><a class="button" href="plugins.php">' . sprintf(__('&larr; Back to %s'), __('Plugins')) . '</a></div>');
