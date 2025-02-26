07fa32098c8d0ecbb27723c1835f2cdb02bc9cd4
Etan Imanol <etanimanolcastro@gmail.com>
2025-02-26 20:05:09 +0000

    ekiline-block-collection

---
+++ ekiline-block-collection/.svn/pristine/8a/8ad15ffa954befa3fffb74a0a2fd92e76aa180b1.svn-base
@@ -0,0 +1,157 @@
+=== Ekiline Block Collection ===
+Contributors:      urielink
+Requires at least: 5.8
+Tags:              block, collection, bootstrap
+Tested up to:      6.7
+Requires PHP:      7.0
+Stable tag:        2.0.1
+License:           GPL-2.0-or-later
+License URI:       https://www.gnu.org/licenses/license-list.html#GPLv2
+Donate link:       https://www.paypal.com/paypalme/urielink
+
+Bootstrap components turned into WordPress editor blocks. Add new functionality to your project. Install and use directly in the editor.
+
+== Description ==
+
+== Made for those of us who love working with Bootstrap ==
+
+- **Accordion**.
+- **Collapse**.
+- **Modal**.
+- **Tabs**.
+- **Progress bar**.
+- **Pop over**.
+- **Tooltip**.
+- **Toast**.
+- **Static carousel**.
+- **Dynamic carousel**.
+- **Offcanvas (new!)**.
+
+
+HTML markup as well as classes and attributes have been respected to work with Bootstrap in its most recent version (5.2.X).
+CSS styles and JS scripts are included, maintaining the layout and behaviors.
+
+= Compatibility =
+Great response on new block-based themes like twenty-twenty two and the twenty-twenty one hybrid themes.
+Do you want more? Download the Ekiline theme to have more options in customizing your site.
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+e.g.
+
+1. Upload the plugin files to the `/wp-content/plugins/ekiline-collection` directory, or install the plugin through the WordPress plugins screen directly.
+1. Activate the plugin through the 'Plugins' screen in WordPress
+
+
+== Frequently Asked Questions ==
+
+= A question that someone might have =
+
+An answer to that question.
+
+= What about foo bar? =
+
+Answer to foo bar dilemma.
+
+== Screenshots ==
+
+1. This screen shot description corresponds to screenshot-1.(png|jpg|jpeg|gif). Note that the screenshot is taken from
+the /assets directory or the directory that contains the stable readme.txt (tags or trunk). Screenshots in the /assets
+directory take precedence. For example, `/assets/screenshot-1.png` would win over `/tags/4.3/screenshot-1.png`
+(or jpg, jpeg, gif).
+2. This is the second screen shot
+
+== Changelog ==
+
+= 2.0.1 =
+* Quick fix: properly initialize loading of plugin styles and scripts
+
+= 2.0.0 =
+* Major update:
+* Refactoring of dynamic carousel due to XSS vulnerability.
+* Text and language adjustment.
+* New information pages added.
+* New options to disable styles and scripts.
+
+= 1.0.7 =
+* Security Fix: XSS vulnerability in dynamic carousel.
+
+= 1.0.6 =
+* Improve: Video behavior, pause when video was in modal or offcanvas container.
+* Fix: Dynamic Carousel, clean shortcodes from content.
+
+= 1.0.5 =
+* Improve: add individual posts in basic carousel.
+
+= 1.0.4 =
+* Fix classname in dynamic carosuel.
+* Improve: Avoid lazy load in dynamic carosuel first image.
+* Fix compatibility carousel shortcode method.
+
+= 1.0.3 =
+* Fix language descriptions.
+
+= 1.0.2 =
+* Allow shortcodes in modal block.
+* Update language files.
+
+= 1.0.1 =
+* Fix modal script.
+
+= 1.0.0 =
+* Refactoring of css styles and js scripts to improve compatibility with themes.
+* New options for the dynamic carousel, choose what to show: images, posts or videos.
+* In addition you will be able to link each image with the use of the media description field.
+
+= 0.1.7 =
+* Improve: Extend link control (images). Add options to trigger collapsing, modal or offcanvas.
+* Enahnce: Extend link control (buttons). Add options to trigger collapsing, modal or offcanvas.
+* Update: Language files.
+
+= 0.1.6 =
+* WP 6.1 Compatibility Check, works fine with new theme: Twenty Twenty-Three.
+* Fix: Toast: title displayed HTML tags.
+* Improve: New Offcanvas Block.
+* Update: Language files.
+
+= 0.1.5 =
+* Fix: Carousel title.
+
+= 0.1.4 =
+* This version is being tested in different online projects, it has not shown difficulties in its operation. It allows adding improvements and new features to the development team.
+* Improve: Extend carousel module, added new visual controls.
+
+= 0.1.3 =
+* Improve: Added information page, admin/themes/About EBC.
+* Fix: Carousel static and Carousel dynamic height styles.
+* Fix: Compatibility problem, editor gets stuck when select an image/media block.
+* Fix: Carousel static, correction when set, add or delete images.
+* Improve: Carousel static added show hide caption.
+
+= 0.1.2 =
+* Improve: Ajust css rules and js for all blocks.
+* Improve: Init for bootstrap items.
+* Update: Readme information.
+
+= 0.1.1 =
+* Update banner and icon images
+
+= 0.1.0 =
+* Release
+
+== Upgrade Notice ==
+
+New carousel design options!
+Link images and activate modal, offcanvas and more!
+
+== About author ==
+
+The author of this plugin will be careful to keep the code as faithful to Bootstrap.
+As well as to maintain this development and incorporate new features in each new update.
+
+“I have created this plugin to provide a better web development service to my clients, from a trusted source such as WordPress.”
+Uri Lazcano.
+
+DevIm: [node v19.1.0, npm 8.19.3, nvm 0.39.3]
---
+++ ekiline-block-collection/ekiline-block-collection.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * Plugin Name:       Ekiline Block Collection
+ * Description:       Actions and blocks based on bootstrap 5 (carousel, collapse and more). Includes Bootstrap library. Support this project to add new features and expand a customer service branch.
+ * Requires at least: 5.8
+ * Requires PHP:      7.0
+ * Version:           2.0.1
+ * Author:            Uri Lazcano (Urielink)
+ * Author URI:        https://ekiline.com/ekiline-block-collection/
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/license-list.html#GPLv2
+ * Text Domain:       ekiline-block-collection
+ *
+ * @package           ekiline-block-collection
+ */
+
+/**
+ * Adds a settings link on the plugins page.
+ */
+function ekiline_block_collection_settings_links( $links_array, $plugin_file_name ){
+    // New link.
+    $site_admin_plugin_page_url = admin_url('admin.php?page=ekiline-block-collection');
+    $new_link = sprintf('<a href="%s">%s</a>', $site_admin_plugin_page_url, __('Settings', 'ekiline-block-collection'));
+
+    // Add to links array.
+	if( strpos( $plugin_file_name, basename( __FILE__ ) ) ) {
+		array_unshift( $links_array, $new_link );
+	}
+	return $links_array;
+}
+add_filter( 'plugin_action_links', 'ekiline_block_collection_settings_links', 25, 2 );
+
+/**
+ * Registers the block using the metadata loaded from the `block.json` file.
+ * Behind the scenes, it registers also all assets so they can be enqueued
+ * through the block editor in the corresponding context.
+ *
+ * @see https://developer.wordpress.org/reference/functions/register_block_type/
+ *
+ * Para incorporar el idioma se requiere una serie de pasos, y crear POT manual.
+ * @link https://developer.wordpress.org/block-editor/how-to-guides/internationalization/
+ * @link https://developer.wordpress.org/cli/commands/i18n/make-json/
+ * El manual indica que hagas un registro del script con dependencias.
+ * Pero, también es posible abstraerlo, se requiere saber el handler del script.
+ */
+function ekiline_collection_ekiline_collection_block_init()
+{
+    // Collection script.
+    register_block_type(__DIR__ . '/build');
+    // Language plugin for PHP.
+    load_plugin_textdomain('ekiline-block-collection', false, basename(dirname(__FILE__)) . '/languages/');
+    // Language plugin for JS Blocks.
+    wp_set_script_translations('ekiline-block-collection-ekiline-block-collection-editor-script', 'ekiline-block-collection', plugin_dir_path(__FILE__) . 'languages');
+}
+add_action('init', 'ekiline_collection_ekiline_collection_block_init');
+
+/**
+ * Scripts and styles in the front end.
+ *
+ * @link https://developer.wordpress.org/reference/functions/wp_script_is/
+ */
+function ekiline_collection_required_scripts()
+{
+    // Condition: If Ekiline is not your theme, enable bootstrap plugins.
+    $theme       = wp_get_theme();
+    $text_domain = 'ekiline-block-collection';
+
+    if ('Ekiline' !== $theme->name || 'Ekiline' !== $theme->parent_theme) {
+        wp_enqueue_style($text_domain . '-bootstrap-style', plugin_dir_url(__FILE__) . 'includes/assets/css/bootstrap.min.css', array(), '5', 'all');
+        wp_enqueue_script($text_domain . '-bootstrap-script', plugin_dir_url(__FILE__) . 'includes/assets/js/bootstrap.bundle.min.js', array(), '5', true);
+    }
+    if ('Ekiline' === $theme->name || 'Ekiline' === $theme->parent_theme) {
+        wp_dequeue_style($text_domain . '-bootstrap-style');
+        wp_dequeue_script($text_domain . '-bootstrap-script');
+    }
+
+    // Condition: If Ekiline is not your theme, enable plugin manager.
+    $style_handler  = $text_domain . '-bootstrap-style';
+    $script_handler = $text_domain . '-bootstrap-script';
+    if('Ekiline' === $theme->name || 'Ekiline' === $theme->parent_theme) {
+        $style_handler  = 'ekiline-style';
+        $script_handler = 'ekiline-layout';
+    }
+    wp_enqueue_style($text_domain . '-block-styles', plugin_dir_url(__FILE__) . 'includes/assets/css/ekiline-collection-block-styles.css', array( $style_handler ), '1.0', 'all');
+    wp_enqueue_script($text_domain . '-block-scripts', plugin_dir_url(__FILE__) . 'includes/assets/js/ekiline-collection-block-scripts.js', array( $script_handler ), '1.0', true);
+
+    // Plugin options.
+    $opcion_plugin_styles  = get_option('ekiline_block_collection_bootstrap_css', '1');
+    $opcion_plugin_scripts = get_option('ekiline_block_collection_bootstrap_js', '1');
+
+    if ($opcion_plugin_styles == '0') {
+        wp_dequeue_style($text_domain . '-bootstrap-style');
+        wp_dequeue_style($text_domain . '-block-styles');
+    }
+    if ($opcion_plugin_scripts == '0') {
+        wp_dequeue_script($text_domain . '-bootstrap-script');
+        wp_dequeue_script($text_domain . '-block-scripts');
+    }
+}
+add_action('wp_enqueue_scripts', 'ekiline_collection_required_scripts', 1);
+
+/**
+ * Other features.
+ * - Plugin information page.
+ * - Carousel script v2.
+ */
+define('EKILINE_COLLECTION_PATH', plugin_dir_path(__FILE__) . 'includes/');
+require EKILINE_COLLECTION_PATH . 'ekiline-collection-info.php';
+require EKILINE_COLLECTION_PATH . 'ekiline-collection-carousel.php';
