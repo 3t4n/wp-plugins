71203ec2d2d5b0131b17f16e493128ed3da8fa3c
Etan Imanol <etanimanolcastro@gmail.com>
2025-02-26 15:16:03 +0000

    site-search-360

---
+++ site-search-360/views/sitesearch360-contact.php
@@ -0,0 +1,11 @@
+<div class="wrapper wrapper--narrow">
+    <div class="block block--first flex flex--center flex--column">
+        <h2><?php esc_html_e('Any questions, ideas, feedback?', 'site-search-360') ?></h2>
+        <div class="flex flex--center">
+            <a class="flex flex--column flex--center m-1" href="mailto:mail@sitesearch360.com?subject=Wordpress Plugin Feedback (siteId: <?php echo get_option('ss360_siteId'); ?>)" title="mail@sitesearch360.com">
+                <img width="83" class="m-b-1" role="presentation" src="<?php echo plugins_url('images/icons/email.svg', dirname(__FILE__)) ?>">
+                <span><?php esc_html_e('write us an email', 'site-search-360') ?></span>
+            </a>    
+        </div>
+    </div>
+</div>
