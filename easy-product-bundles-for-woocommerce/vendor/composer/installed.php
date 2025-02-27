ce725e55616ce734c1a01a549921be06e82ff8aa
Etan Imanol <etanimanolcastro@gmail.com>
2025-02-27 03:38:45 +0000

    Batch commit: everest-backup, auto-hide-admin-bar, wpc-name-your-price, mwb-bookings-for-woocommerce, wpc-product-options, popup-box, different-menus-in-different-pages, woo-thank-you-page-customizer, easy-product-bundles-for-woocommerce, smntcs-disable-rest-api-user-endpoints, disable-gutenberg-blocks, very-simple-meta-description, import-eventbrite-events, related-posts-flatsome, link-widget-title, luckywp-scripts-control, wp-swiper, wp-posts-carousel, watu, translate-words

---
+++ easy-product-bundles-for-woocommerce/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'asanaplugins/easy-product-bundles-woocommerce',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '187ebb58a204b49b95cf68b6f0a68d58f76de822',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'asanaplugins/easy-product-bundles-woocommerce' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '187ebb58a204b49b95cf68b6f0a68d58f76de822',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
