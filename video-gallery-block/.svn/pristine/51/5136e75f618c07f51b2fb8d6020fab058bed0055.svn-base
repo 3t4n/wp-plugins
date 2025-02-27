e67e554d846c6b4ab2b61b980838a1d0818cef2b
Etan Imanol <etanimanolcastro@gmail.com>
2025-02-27 02:49:56 +0000

    Batch commit: video-gallery-block, blog-posts-and-category-for-elementor, owl-carousel-wp, gd-rating-system, rescue-shortcodes, tabs-pro, cal-embedder-lite, moosend-email-marketing, products-compare-for-woocommerce, b-social-share, senangpay-payment-gateway-for-woocommerce, post-type-archive-descriptions, litcommerce, thinker-sidebar-shortcode, attesa-extra, call-to-action-block-wppool, invite-anyone, wp-edit-username, resizable-editor-sidebar, lazy-facebook-comments

---
+++ video-gallery-block/.svn/pristine/51/5136e75f618c07f51b2fb8d6020fab058bed0055.svn-base
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => 'd61dc438abb237093c7f');
---
+++ video-gallery-block/index.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Plugin Name: Video Gallery Block
+ * Description: Display your videos as gallery in a professional way.
+ * Version: 1.1.0
+ * Author: bPlugins
+ * Author URI: https://bplugins.com
+ * License: GPLv3
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.txt
+ * Text Domain: video-gallery
+ */
+
+// ABS PATH
+if ( !defined( 'ABSPATH' ) ) { exit; }
+
+// Constant
+define( 'VGB_PLUGIN_VERSION', isset( $_SERVER['HTTP_HOST'] ) && 'localhost' === $_SERVER['HTTP_HOST'] ? time() : '1.1.0' );
+define( 'VGB_DIR_URL', plugin_dir_url( __FILE__ ) );
+define( 'VGB_PUBLIC_DIR', VGB_DIR_URL . 'public/' );
+define( 'VGB_DIR_PATH', plugin_dir_path( __FILE__ ) );
+
+if( !class_exists( 'VGBPlugin' ) ) {
+	class VGBPlugin{
+		function __construct(){
+			add_action( 'enqueue_block_assets', [$this, 'enqueueBlockAssets'] );
+			add_action( 'wp_enqueue_scripts', [$this, 'wpEnqueueScripts'] );
+			add_action( 'init', [$this, 'onInit'] );
+		}
+	
+		function enqueueBlockAssets(){
+			wp_register_script( 'isotope', VGB_PUBLIC_DIR . 'js/isotope.pkgd.min.js', [], '3.0.6', true );
+		}
+	
+		function wpEnqueueScripts(){
+			wp_register_script( 'plyr', VGB_PUBLIC_DIR . 'js/plyr.js', [], '3.7.2', true );
+			wp_register_style( 'plyr', VGB_PUBLIC_DIR . 'css/plyr.css', [], '3.7.2' );
+		}
+	
+		function onInit() {
+			register_block_type( __DIR__ . '/build' );
+		}
+	}
+	new VGBPlugin();
+}
