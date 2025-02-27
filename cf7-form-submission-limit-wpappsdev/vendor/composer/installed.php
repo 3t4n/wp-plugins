615644971776a03d9908120eee195ba87770ab3f
Etan Imanol <etanimanolcastro@gmail.com>
2025-02-27 03:32:54 +0000

    Batch commit: 3d-viewer-block, import-xml-csv-settings-to-all-in-one-seo-pack, simple-googlebot-visit, yandex-share, cf7-form-submission-limit-wpappsdev, image-widget-deluxe, wp-site-verification-tool, woocommerce-and-1centerprise-data-exchange, bp-activity-plus-reloaded, wp-notification-center, postepay-woocommerce-gateway, baselinker-woo, fat-rat-collect, litextension-data-migration-to-woocommerce, product-page-shipping-calculator-for-woocommerce, pageview, advanced-wplink, related-products-manager-woocommerce, show-all-pages-url, blox-lite

---
+++ cf7-form-submission-limit-wpappsdev/vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'name' => 'siananda/wpappsdev-submission-limit-cf7',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'e6dd8c22d19e326b9d75f53f673e2ffda0b464a3',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'appsero/client' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => 'aef4cf138ab4f348cee4e2f20f7d3cd815db0877',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../appsero/client',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'siananda/wpappsdev-submission-limit-cf7' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'e6dd8c22d19e326b9d75f53f673e2ffda0b464a3',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
