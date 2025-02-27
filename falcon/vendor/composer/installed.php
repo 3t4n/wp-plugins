ee624f1fd86620771fef60e63b4bb4731b7ba4a8
Etan Imanol <etanimanolcastro@gmail.com>
2025-02-27 03:42:32 +0000

    Batch commit: tc-logo-slider, fullworks-anti-spam, image-title-remove, rcd-right-click-disabler, liveblog, dofollow-case-by-case, wc-speed-drain-repair, prevent-landscape-rotation, no-comments-on-pages, restropress, first-order-discount-woocommerce, disable-url-autocorrect-guessing, dinosaur-game, disable-wp-registration-page-spam, post-reading-times, custom-block-patterns, falcon, email-notification-on-login, wpecounter, atarim-visual-collaboration

---
+++ falcon/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'elightup/falcon',
+        'pretty_version' => '2.8.5',
+        'version' => '2.8.5.0',
+        'reference' => 'eec4212650a8aedd612036e39d93004a662ca3d8',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'elightup/falcon' => array(
+            'pretty_version' => '2.8.5',
+            'version' => '2.8.5.0',
+            'reference' => 'eec4212650a8aedd612036e39d93004a662ca3d8',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
