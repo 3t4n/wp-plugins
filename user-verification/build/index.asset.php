00e695a8c20c2ce4056dcd9ed89e4d89f455e601
Etan Imanol <etanimanolcastro@gmail.com>
2025-02-27 04:18:51 +0000

    Batch commit: document-embedder-addons-for-elementor, instapage, plugin-detective, themify-builder, mediapress, smart-wishlist-for-more-convert, publishpress, rel-nofollow-checkbox, user-verification, bst-dsgvo-cookie, country-state-city-auto-dropdown, front-end-pm, list-categories, food-and-drink-menu, no-captcha-recaptcha, wp-comment-policy-checkbox, posts-like-dislike, kiwi-social-share, list-pages-shortcode, i-recommend-this

---
+++ user-verification/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => 'f808416261957cd4e51e');
