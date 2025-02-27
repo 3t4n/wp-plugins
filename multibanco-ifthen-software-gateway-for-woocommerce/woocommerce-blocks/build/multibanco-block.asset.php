3ff00e71fabbfef0b1b56fca355b4c8dd0cbf571
Etan Imanol <etanimanolcastro@gmail.com>
2025-02-27 04:08:21 +0000

    Batch commit: responsivevoice-text-to-speech, simple-membership-custom-messages, themeinwp-import-companion, emails-verification-for-woocommerce, wp-migration-duplicator, if-so, idx-broker-platinum, track-the-click, woo-conditional-product-fees-for-checkout, product-visibility-by-user-role-for-woocommerce, coming-soon-maintenance-mode, unlimited-elementor-inner-sections-by-boomdevs, multibanco-ifthen-software-gateway-for-woocommerce, wp-fail2ban-redux, social-media-auto-publish, wp-auto-updater, genesis-portfolio-pro, itro-popup, gsheetconnector-wpforms, onelogin-saml-sso

---
+++ multibanco-ifthen-software-gateway-for-woocommerce/woocommerce-blocks/build/multibanco-block.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wc-blocks-registry', 'wc-settings', 'wp-html-entities', 'wp-i18n'), 'version' => 'b8f5a326fb8ad78f9748');
---
+++ multibanco-ifthen-software-gateway-for-woocommerce/woocommerce-blocks/build/payshop-block.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wc-blocks-registry', 'wc-settings', 'wp-html-entities', 'wp-i18n'), 'version' => '412a73ba1b14a4a7bd21');
