1bf8fe90e73f5c9e8a7476f236881558784e5493
Etan Imanol <etanimanolcastro@gmail.com>
2025-02-27 03:25:53 +0000

    Batch commit: append-link-on-copy, recent-posts-flexslider, spots, sx-no-homepage-pagination, send-email-from-admin, custom-css-and-js, ai-copilot, pdf-builder-for-wpforms, themx-maximum-upload-file-size, themify-audio-dock, payment-gateway-via-borgun-for-woocommerce, wp-mailgun-smtp, use-administrator-password, autopost-to-mastodon, passwordless-login, wordpress-hit-counter, taxonomy-tinymce, unit-price-for-woocommerce, styled-calendar-customized-google-calendars, off-canvas-sidebars

---
+++ pdf-builder-for-wpforms/DTO/TableControl.php
@@ -0,0 +1,34 @@
+<?php
+namespace rednaoformpdfbuilder\DTO;
+class FieldControlWrapper{
+	 /** @var FieldControlOptions */
+	 public $Field;
+}
+
+
+class ColumnItemOptions{
+	 /** @var PDFControlBaseOptions[] */
+	 public $Fields;
+	 public $Width;
+     public $Alignment;
+}
+
+
+class RowItemOptions{
+	 /** @var ColumnItemOptions[] */
+	 public $Columns;
+}
+
+
+class TableItemOptions{
+	 /** @var RowItemOptions[] */
+	 public $Rows;
+}
+
+
+class TableControlOptions{
+	 /** @var TableItemOptions */
+	 public $TableItem;
+}
+
+
