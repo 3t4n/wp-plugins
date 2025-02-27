8e0fd7083aea8ef639ec4dba9c9d7486a3b403cd
Etan Imanol <etanimanolcastro@gmail.com>
2025-02-27 04:18:21 +0000

    Batch commit: wp-mailfrom-ii, front-page-category, native-emoji, whmcs-bridge, image-hover-effects-block, elemailer-lite, disable-plugin-autoupdate-emails, ambrosite-nextprevious-post-link-plus, payu-india, multiple-roles, simple-website-redirect, widget-visibility-without-jetpack, wp-private-content-plus, custom-links-in-elementor-image-carousel, flip-boxes, wp-cafe, jquery-lightbox-for-native-galleries, simple-file-list, admin-management-xtended, event-page-templates-addon-for-the-events-calendar

---
+++ simple-website-redirect/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wpscholar/simple-website-redirect',
+        'pretty_version' => '1.3.1',
+        'version' => '1.3.1.0',
+        'reference' => '21b64fd50f55c2af5c39a902399b0833ea5695ea',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'wpscholar/simple-website-redirect' => array(
+            'pretty_version' => '1.3.1',
+            'version' => '1.3.1.0',
+            'reference' => '21b64fd50f55c2af5c39a902399b0833ea5695ea',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wpscholar/url' => array(
+            'pretty_version' => '1.2.3',
+            'version' => '1.2.3.0',
+            'reference' => '6e97376a1cf3a9f4d0f0ae976cece6123d1bd4b8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wpscholar/url',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
