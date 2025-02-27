b2edc5fca38cd6a84b63ac76978c3a5c33bfa70d
Etan Imanol <etanimanolcastro@gmail.com>
2025-02-27 03:07:22 +0000

    Batch commit: ali2woo-lite, sender-net-automated-emails, anspress-question-answer, attachment-importer, wp-twitter-feeds, filter-gallery, widgetize-pages-light, wp-simple-mail-sender, noindex-seo, zoho-campaigns, helper-lite-for-pagespeed, chatra-live-chat, wp-attachments, slider-block, slicknav-mobile-menu, superb-tables, sitewide-notice-wp, wc-gsheetconnector, custom-shipping-methods-for-woocommerce, image-uploader-for-welcart

---
+++ helper-lite-for-pagespeed/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'seojacky/helper-lite-for-pagespeed',
+        'pretty_version' => '3.1.7',
+        'version' => '3.1.7.0',
+        'reference' => 'b884dc4fc8b12704c6836d5d0451599e5094d5bb',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'seojacky/helper-lite-for-pagespeed' => array(
+            'pretty_version' => '3.1.7',
+            'version' => '3.1.7.0',
+            'reference' => 'b884dc4fc8b12704c6836d5d0451599e5094d5bb',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
