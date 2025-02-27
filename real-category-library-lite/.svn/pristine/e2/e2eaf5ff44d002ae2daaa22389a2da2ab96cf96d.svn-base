1b9eec0d9dad6deaf8ac9c46f2ceeca05c584940
Etan Imanol <etanimanolcastro@gmail.com>
2025-02-27 01:34:00 +0000

    Batch commit: simple-membership-form-shortcode, sanitize-spanish-filenames, back-to-the-top-button, ovation-elements, post-slider-for-elementor, genesis-simple-menus, beautiful-taxonomy-filters, spiffy-calendar, database-for-cf7, post-layouts, wp-first-letter-avatar, wp-twilio-core, media-slider, debug, real-category-library-lite, wpc-product-size-chart, disable-woocommerce-reviews, matchheight, wc-wise-gateway, the-courier-guy

---
+++ real-category-library-lite/.svn/pristine/e2/e2eaf5ff44d002ae2daaa22389a2da2ab96cf96d.svn-base
@@ -0,0 +1,12 @@
+<?php
+// Cachebusters generated on 2025-02-06 06:50:05
+return [
+	'@ant-design/icons' => '5.3.7',
+	'antd' => '5.18.1',
+	'core-js' => '3.37.1',
+	'jquery' => '3.7.1',
+	'mobx' => '4.15.7',
+	'mobx-react' => '6.3.1',
+	'react' => '18.3.1',
+	'react-dom' => '18.3.1'
+];
---
+++ real-category-library-lite/inc/base/others/cachebuster.php
@@ -0,0 +1,30 @@
+<?php
+// Cachebusters generated on 2025-02-06 06:50:44
+return [
+	'src/public/dist/admin.css'=> '6a14d6224035761c5d52f69f0b59d03f',
+	'src/public/dist/admin.css.map'=> 'ac0a8add2475814b679aa27a3f0a3292',
+	'src/public/dist/admin.lite.js'=> 'f84fae9747503f45f2ed4cef2eefe75c',
+	'src/public/dist/admin.lite.js.map'=> '3421dafad419073435565ccaff78e7f8',
+	'src/public/dist/admin.lite.js.pot'=> '646dfcc281497f49d1d992f04c7ec250',
+	'src/public/dist/admin.pro.js'=> 'c266c7c3fb21d5b22cefc8d7250979f4',
+	'src/public/dist/admin.pro.js.map'=> 'a4d9804d058463388454ecc2140e6d1d',
+	'src/public/dist/admin.pro.js.pot'=> '1c4fabe0bb359f9be82821c621614612',
+	'src/public/dist/i18n-dependency-map-default-lite.json'=> '99914b932bd37a50b983c5e7c90ae93b',
+	'src/public/dist/i18n-dependency-map-default-pro.json'=> '99914b932bd37a50b983c5e7c90ae93b',
+	'src/public/dist/options.css'=> '6776e55c71abe65fefc05ecaff08b611',
+	'src/public/dist/options.css.map'=> '9dea0a728def6b68fdc9d1a49db457ce',
+	'src/public/dist/options.lite.js'=> 'ca9d8696683689ddb0717fc4e8437b99',
+	'src/public/dist/options.lite.js.map'=> '66578436436dc36758c019e436f3e9f8',
+	'src/public/dist/options.lite.js.pot'=> '3de56e76915f85fab50c5f1d33b2501d',
+	'src/public/dist/options.pro.js'=> 'c74acb0f3751f7d49f1066b9dbc19ffc',
+	'src/public/dist/options.pro.js.map'=> '109ef20972167f039a66e0e2f2bf2059',
+	'src/public/dist/options.pro.js.pot'=> 'fb9d216acae775c281ba682d2e9dfb66',
+	'src/public/dist/vendor-admin.lite.js'=> '47b42f39a8c8686b80b21525612cea53',
+	'src/public/dist/vendor-admin.lite.js.LICENSE.txt'=> 'e73c0120c07324eb4123e7a817b256d0',
+	'src/public/dist/vendor-admin.pro.js'=> '2c5bf641bdc1847894fed319868b62a6',
+	'src/public/dist/vendor-admin.pro.js.LICENSE.txt'=> 'e73c0120c07324eb4123e7a817b256d0',
+	'src/public/dist/vendor-options.lite.js'=> 'c0e2553b741c3471bee38c20574823e3',
+	'src/public/dist/vendor-options.lite.js.LICENSE.txt'=> 'a1c47e27bca7e25af48e23598a310560',
+	'src/public/dist/vendor-options.pro.js'=> '22dde06cd9b8da0b04d3f5deed481d41',
+	'src/public/dist/vendor-options.pro.js.LICENSE.txt'=> 'a1c47e27bca7e25af48e23598a310560'
+];
