3b7a62e77d8c7b8ab00bfc59658dcdb6a8aaadea
Etan Imanol <etanimanolcastro@gmail.com>
2025-02-27 01:24:43 +0000

    Batch commit: woocommerce-easy-table-rate-shipping, paid-member-subscriptions, wp-data-access, website-article-monetization-by-magenet, sp-news-and-widget, disable-embeds, charitable, best-woocommerce-feed, events-widgets-for-elementor-and-the-events-calendar, woocommerce-product-tabs, ics-calendar, wp-subtitle, use-google-libraries, social-icons, robots-txt-editor, shortpixel-adaptive-images, advanced-tinymce-configuration, show-hidecollapse-expand, wp-accessibility-helper, ari-fancy-lightbox

---
+++ best-woocommerce-feed/vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => 'rextheme/best-woocommerce-feed',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '49b6d1f26333a41b0c8658f2a74ae862e211bbec',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'appsero/client' => array(
+            'pretty_version' => 'v2.0.4',
+            'version' => '2.0.4.0',
+            'reference' => '12ff65b9770286d21edf314e7acfcd26fdde3315',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../appsero/client',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'philipnewcomer/wp-ajax-helper' => array(
+            'pretty_version' => '1.0.1',
+            'version' => '1.0.1.0',
+            'reference' => '3416943af039cbca4eba1efbf4d9f728860e3a5e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../philipnewcomer/wp-ajax-helper',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'rextheme/best-woocommerce-feed' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '49b6d1f26333a41b0c8658f2a74ae862e211bbec',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
