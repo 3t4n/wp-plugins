7ce110f7262abeb962c2cc62ce7b269f7e2c0d93
Etan Imanol <etanimanolcastro@gmail.com>
2025-02-27 03:45:40 +0000

    Batch commit: wp-nav-plus, acf-color-swatches, custom-css-editor, date-time-picker-for-wpforms, comment-moderation-e-mail-to-post-author, if-widget, customizer-login-page, advanced-sermons, private-google-calendars, export-post-info, pw-woocommerce-exclude-free-shipping, admin-customizer, formfacade, notix-web-push-notifications, wc-serial-numbers, material-admin-theme, pt-theme-addon, smart-archives-reloaded, wp-vertical-image-slider, visual-verse-of-the-day-widget

---
+++ wc-serial-numbers/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'pluginever/wc-serial-numbers',
+        'pretty_version' => 'v2.1.4',
+        'version' => '2.1.4.0',
+        'reference' => '1079e6a409e914e4270f6decc923d2f2280a36ae',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'pluginever/wc-serial-numbers' => array(
+            'pretty_version' => 'v2.1.4',
+            'version' => '2.1.4.0',
+            'reference' => '1079e6a409e914e4270f6decc923d2f2280a36ae',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
